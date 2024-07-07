8f5c5d8534fa6b9a14e29ca401ed3290e869b163
Oliver P. <GitHub@opott.is-cool.me>
2024-07-07 12:46:55 +0000

    chore: Add dynamic header and footer components

---
+++ headerfooter.js
@@ -0,0 +1,35 @@
+class DynHeader extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+          <link rel="stylesheet" href="/styles.css">
+          <header>
+          <h1>V-Cook</h1>
+          <nav>
+              <ul>
+                  <li><a href="/index.html">Home</a></li>
+              </ul>
+          </nav>
+          </header>
+          `;
+    }
+  }
+  
+  class DynFooter extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+          <link rel="stylesheet" href="/styles.css">
+          <footer>
+            <nav>
+                <ul>
+                    <li><a href="https://github.com/opott" target="_blank">GitHub</a></li>
+                    <li><a href="https://www.instagram.com/ompot09/" target="_blank">Instagram</a></li>
+                </ul>
+            </nav>
+              <p>&copy; 2024 Oliver Potter. All rights reserved.</p>
+          </footer>
+          `;
+    }
+  }
+  
+  customElements.define("dyn-header", DynHeader);
+  customElements.define("dyn-footer", DynFooter);
